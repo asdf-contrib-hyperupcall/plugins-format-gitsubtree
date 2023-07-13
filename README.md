e133a8e02c82c72b4a3d6a2a01d8903fac97e6a7
captain-woofers <99463792+mister-woofers@users.noreply.github.com>
2023-07-13 05:08:39 -0700

    Squashed 'repos/asdf-yj/' content from commit e97b5ca9
    git-subtree-dir: repos/asdf-yj
    git-subtree-split: e97b5ca936f130aba597c84f95acc61b81ba907b

---
+++ README.md
@@ -0,0 +1,30 @@
+<div align="center">
+
+# asdf-yj [![Build](https://github.com/ryodocx/asdf-yj/actions/workflows/build.yml/badge.svg)](https://github.com/ryodocx/asdf-yj/actions/workflows/build.yml) [![Lint](https://github.com/ryodocx/asdf-yj/actions/workflows/lint.yml/badge.svg)](https://github.com/ryodocx/asdf-yj/actions/workflows/lint.yml)
+
+[yj](https://github.com/sclevine/yj) plugin for the [asdf version manager](https://asdf-vm.com).
+
+</div>
+
+# Install
+
+Plugin:
+
+```shell
+asdf plugin add yj
+# or
+asdf plugin add yj https://github.com/ryodocx/asdf-yj.git
+```
+
+yj:
+
+```shell
+# Install latest version
+asdf install yj latest
+
+# Set a version globally (on your ~/.tool-versions file)
+asdf global yj latest
+```
+
+Check [asdf](https://github.com/asdf-vm/asdf) readme for more instructions on how to
+install & manage versions.
---
+++ lib/utils.bash
@@ -0,0 +1,88 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+GH_REPO="https://github.com/sclevine/yj"
+TOOL_NAME="yj"
+TOOL_TEST="yj -h"
+
+fail() {
+  echo -e "asdf-$TOOL_NAME: $*"
+  exit 1
+}
+
+curl_opts=(-fsSL)
+
+if [ -n "${GITHUB_API_TOKEN:-}" ]; then
+  curl_opts=("${curl_opts[@]}" -H "Authorization: token $GITHUB_API_TOKEN")
+fi
+
+sort_versions() {
+  sed 'h; s/[+-]/./g; s/.p\([[:digit:]]\)/.z\1/; s/$/.z/; G; s/\n/ /' |
+    LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n | awk '{print $2}'
+}
+
+list_github_tags() {
+  git ls-remote --tags --refs "$GH_REPO" |
+    grep -o 'refs/tags/.*' | cut -d/ -f3- |
+    sed 's/^v//'
+}
+
+list_all_versions() {
+  list_github_tags
+}
+
+download_release() {
+  local version filename url
+  version="$1"
+  filename="$2"
+
+  case $(uname -s) in
+  Linux)
+    case $(uname -m) in
+    x86_64) suffix="-linux-amd64" ;;
+    arm64) suffix="-linux-arm64" ;;
+    armv7l) suffix="-linux-arm-v7" ;;
+    armv5l) suffix="-linux-arm-v5" ;;
+    *) fail "Unsupported architecture: $(uname -m)" ;;
+    esac
+    ;;
+  Darwin)
+    case $(uname -m) in
+    x86_64) suffix="-macos-amd64" ;;
+    arm64) suffix="-macos-arm64" ;;
+    *) fail "Unsupported architecture: $(uname -m)" ;;
+    esac
+    ;;
+  *) fail "Unsupported platform: $(uname -s)" ;;
+  esac
+
+  url="$GH_REPO/releases/download/v${version}/yj${suffix}"
+
+  echo "* Downloading $TOOL_NAME release $version..."
+  curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
+}
+
+install_version() {
+  local install_type="$1"
+  local version="$2"
+  local install_path="$3"
+
+  if [ "$install_type" != "version" ]; then
+    fail "asdf-$TOOL_NAME supports release installs only"
+  fi
+
+  (
+    mkdir -p "$install_path/bin"
+    cp "$ASDF_DOWNLOAD_PATH/$TOOL_NAME" "$install_path/bin/"
+
+    local tool_cmd
+    tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
+    test -x "$install_path/bin/$tool_cmd" || fail "Expected $install_path/bin/$tool_cmd to be executable."
+
+    echo "$TOOL_NAME $version installation was successful!"
+  ) || (
+    rm -rf "$install_path"
+    fail "An error ocurred while installing $TOOL_NAME $version."
+  )
+}
