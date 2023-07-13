d601e296154313afde369eba4fd28a7243f9d226
captain-woofers <99463792+mister-woofers@users.noreply.github.com>
2023-07-13 05:00:17 -0700

    Squashed 'repos/asdf-purescript/' content from commit 24813a09
    git-subtree-dir: repos/asdf-purescript
    git-subtree-split: 24813a09f6d07b5ab945bbf8a1c6b4401728901a

---
+++ README.md
@@ -0,0 +1,40 @@
+<div align="center">
+
+# asdf-purescript ![Build](https://github.com/nsaunders/asdf-purescript/workflows/Build/badge.svg) ![Lint](https://github.com/nsaunders/asdf-purescript/workflows/Lint/badge.svg)
+
+[PureScript](https://github.com/purescript/documentation) plugin for the [asdf version manager](https://asdf-vm.com).
+
+</div>
+
+# Dependencies
+
+The only dependencies are `bash`, `curl`, and `tar`.
+
+# Install
+
+Plugin:
+
+```shell
+asdf plugin-add purescript
+# or
+asdf plugin-add https://github.com/nsaunders/asdf-purescript.git
+```
+
+Installation:
+
+```shell
+# Show all installable versions.
+asdf list-all purescript
+
+# Install specific version.
+asdf install purescript latest
+
+# Set a version globally (on your ~/.tool-versions file).
+asdf global purescript latest
+
+# Now purs command is available.
+purs --help
+```
+
+Check [asdf](https://github.com/asdf-vm/asdf) readme for more instructions on how to
+install and manage versions.
---
+++ lib/utils.bash
@@ -0,0 +1,76 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+GH_REPO="https://github.com/purescript/purescript"
+
+fail() {
+  echo -e "asdf-purescript: $*"
+  exit 1
+}
+
+curl_opts=(-fsSL)
+
+if [ -n "${GITHUB_API_TOKEN:-}" ]; then
+  curl_opts=("${curl_opts[@]}" -H "Authorization: token $GITHUB_API_TOKEN")
+fi
+
+sort_versions() {
+  sed 'h; s/[+-]/./g; s/.p\([[:digit:]]\)/.z\1/; s/$/.z/; G; s/\n/ /' |
+    LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n | awk '{print $2}'
+}
+
+list_github_tags() {
+  git ls-remote --tags --refs "$GH_REPO" |
+    grep -o 'refs/tags/.*' | cut -d/ -f3- |
+    sed 's/^v//'
+}
+
+list_all_versions() {
+  list_github_tags
+}
+
+download_release() {
+  local version filename bin url
+  version="$1"
+  filename="$2"
+
+  if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    bin="linux64"
+  elif [[ "$OSTYPE" == "darwin"* ]]; then
+    bin="macos"
+  else
+    fail "unrecognized operating system $OSTYPE"
+  fi
+
+  url="$GH_REPO/releases/download/v${version}/${bin}.tar.gz"
+
+  echo "* Downloading purescript release $version..."
+  curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
+}
+
+install_version() {
+  local install_type="$1"
+  local version="$2"
+  local install_path="$3"
+
+  if [ "$install_type" != "version" ]; then
+    fail "asdf-purescript supports release installs only"
+  fi
+
+  local release_file="$install_path/purs-$version.tar.gz"
+  (
+    mkdir -p "$install_path/bin"
+    download_release "$version" "$release_file"
+    tar -xzf "$release_file" -C "$install_path" --strip-components=1 purescript/purs || fail "Could not extract $release_file"
+    mv "$install_path/purs" "$install_path/bin"
+    rm "$release_file"
+
+    test -x "$install_path/bin/purs" || fail "Expected $install_path/bin/purs to be executable."
+
+    echo "purs $version installation was successful!"
+  ) || (
+    rm -rf "$install_path"
+    fail "An error ocurred while installing purescript $version."
+  )
+}
