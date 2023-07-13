135db2d3e2c793c2b2b161e8fdfc423fe9f52410
captain-woofers <99463792+mister-woofers@users.noreply.github.com>
2023-07-13 05:08:14 -0700

    Squashed 'repos/asdf-xchtmlreport/' content from commit 139fb861
    git-subtree-dir: repos/asdf-xchtmlreport
    git-subtree-split: 139fb8614587737c6bc00481cd36173d7ca1ba78

---
+++ lib/utils.bash
@@ -0,0 +1,74 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+GH_REPO="https://github.com/XCTestHTMLReport/XCTestHTMLReport"
+TOOL_NAME="xchtmlreport"
+TOOL_TEST="xchtmlreport -h"
+
+fail() {
+  echo -e "asdf-$TOOL_NAME: $*"
+  exit 1
+}
+
+curl_opts=(-fsSL)
+
+if [ -n "${GITHUB_API_TOKEN:-}" ]; then
+  curl_opts=("${curl_opts[@]}" -H "Authorization: token $GITHUB_API_TOKEN")
+fi
+
+sort_versions() {
+  sed 'h; s/[+-]/./g; s/.p\([[:digit:]]\)/.z\1/; s/$/.z/; G; s/\n/ /' |
+    LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n | awk '{print $2}'
+}
+
+list_github_tags() {
+  git ls-remote --tags --refs "$GH_REPO" |
+    grep -o 'refs/tags/.*' | cut -d/ -f3- |
+    sed 's/^v//'
+}
+
+list_all_versions() {
+  # min. supported version is 2.3.0 for binary installations
+  list_github_tags | awk '$0 >= "2.3.0"'
+}
+
+download_release() {
+  local version filename url
+  version="$1"
+  filename="$2"
+
+  url="$GH_REPO/releases/download/${version}/${TOOL_NAME}-${version}.zip"
+
+  echo "* Downloading $TOOL_NAME release $version..."
+  curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
+}
+
+install_version() {
+  local install_type="$1"
+  local version="$2"
+  local install_path="$3"
+
+  if [ "$install_type" != "version" ]; then
+    fail "asdf-$TOOL_NAME supports release installs only"
+  fi
+
+  (
+    mkdir -p "$install_path"
+    cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
+    cd "$install_path"
+    if [ ! -d bin ]; then
+      mkdir bin >/dev/null 2>&1
+    fi
+    mv "release/${TOOL_NAME}" ./bin || fail "Could not move ${TOOL_NAME} to ./bin folder"
+
+    local tool_cmd
+    tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
+    test -x "$install_path/bin/$tool_cmd" || fail "Expected $install_path/bin/$tool_cmd to be executable."
+
+    echo "$TOOL_NAME $version installation was successful!"
+  ) || (
+    rm -rf "$install_path"
+    fail "An error ocurred while installing $TOOL_NAME $version."
+  )
+}
