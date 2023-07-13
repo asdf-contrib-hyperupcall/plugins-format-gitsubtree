b2261c58d1ad197c62f94046bc0c7e06923d8c32
captain-woofers <99463792+mister-woofers@users.noreply.github.com>
2023-07-13 04:54:24 -0700

    Squashed 'repos/asdf-ibmcloud/' content from commit 9a31772
    git-subtree-dir: repos/asdf-ibmcloud
    git-subtree-split: 9a31772e9950e89db876edb4ca44eccc26ac555a

---
+++ lib/utils.bash
@@ -0,0 +1,103 @@
+#!/usr/bin/env bash
+
+[ "${BASH_VERSINFO[0]}" -ge 3 ] && set -o pipefail
+
+GH_REPO="https://github.com/IBM-Cloud/ibm-cloud-cli-release"
+
+TOOL_NAME="ibmcloud"
+TOOL_TEST="ibmcloud"
+
+get_platform() {
+  local silent=${1:-}
+  local platform=""
+
+  platform="$(uname | tr '[:upper:]' '[:lower:]')"
+
+  case "$platform" in
+  linux | darwin | freebsd)
+    # All good
+    ;;
+  *)
+    fail "Platform '${platform}' not supported!"
+    ;;
+  esac
+
+  printf "%s" "$platform"
+}
+
+get_arch() {
+  local arch=""
+  local arch_check=${ASDF_IBMCLOUD_OVERWRITE_ARCH:-"$(uname -m)"}
+  case "${arch_check}" in
+  x86_64 | amd64) arch="amd64" ;;
+  i686 | i386 | 386) arch="386" ;;
+  armv6l | armv7l) arch="armv6l" ;;
+  aarch64 | arm64) arch="arm64" ;;
+  ppc64le) arch="ppc64le" ;;
+  *)
+    fail "Arch '${arch_check}' not supported!"
+    ;;
+  esac
+
+  printf "%s" "$arch"
+}
+
+release_file() {
+  local ext
+  local arch="$(get_arch)"
+  local platform="$(get_platform)"
+
+  case $platform in
+  linux | freebsd)
+    ext='tar.gz'
+    ;;
+  darwin)
+    ext='pkg'
+    ;;
+  esac
+
+  local prefix="IBM_Cloud_CLI_"
+
+  if [[ $platform == "darwin" ]]; then
+    if [[ $arch == "amd64" ]]; then
+      printf "%s" "${prefix}${ASDF_INSTALL_VERSION}.${ext}"
+    elif [[ $arch == "arm64" ]]; then
+      printf "%s" "${prefix}${ASDF_INSTALL_VERSION}_${arch}.${ext}"
+    else
+      fail "Arch '${arch}' on Darwin not supported!"
+    fi
+  else
+    printf "%s" "${prefix}${ASDF_INSTALL_VERSION}_${arch}.${ext}"
+  fi
+}
+
+release_url() {
+  local base="https://download.clis.cloud.ibm.com/ibm-cloud-cli"
+
+  printf "%s" "${base}/${ASDF_INSTALL_VERSION}/$(release_file)"
+}
+
+sort_versions() {
+  sed 'h; s/[+-]/./g; s/.p\([[:digit:]]\)/.z\1/; s/$/.z/; G; s/\n/ /' |
+    LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n | awk '{print $2}'
+}
+
+list_github_tags() {
+  git ls-remote --tags --refs "$GH_REPO" |
+    grep -o 'refs/tags/.*' | cut -d/ -f3- |
+    sed 's/^v//' |
+    sort_versions
+}
+
+msg() {
+  echo -e "\033[32m$1\033[39m" >&2
+}
+
+err() {
+  echo -e "\033[31m$1\033[39m" >&2
+}
+
+fail() {
+  err "$1"
+  exit 1
+}
